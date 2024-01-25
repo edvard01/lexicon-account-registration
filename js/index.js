8dd92c463d0e9cefd6b866ac4cd7ed0df4aa3cfb
edvard01 <frejaclueless@gmail.com>
2024-01-25 14:53:23 +0100

    Changed the structure of my html, added a onclick event for all my labels, when you click them the corresponding input is focused.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 const btn = document.getElementById("submit-button");
+const label = document.querySelectorAll("label");
+
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Hi :)");
+  let name = document.getElementById("name").value;
+  console.log(name);
+});
+
+label.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let labelId = e.target.getAttribute("id");
+    let index = labelId.indexOf("-");
+    let id = labelId.slice(0, index);
+    console.log(id);
+
+    document.getElementById(id).focus();
+  });
 });
