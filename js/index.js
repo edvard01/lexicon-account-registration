b62c6f242ba82995e52dcf1d03681dd53f7d0ab7
edvard01 <frejaclueless@gmail.com>
2024-01-25 16:09:35 +0100

    Finished all validation, changed styling a bit to accomodate for error messages. Changed some types and id's in the HTML for easier development

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 const btn = document.getElementById("submit-button");
 const label = document.querySelectorAll("label");
+const statusElement = document.getElementById("status-message");
+
+const nameInput = document.getElementById("name");
+const userNameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const pwInput = document.getElementById("pw");
+const pwRepeatInput = document.getElementById("repeatpw");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   let name = document.getElementById("name").value;
   console.log(name);
+
+  let validated = validateInputs();
+  if (validated) {
+    statusElement.innerHTML = "Your account has been signed up!";
+  }
 });
 
 label.forEach((element) => {
@@ -17,3 +29,79 @@ label.forEach((element) => {
     document.getElementById(id).focus();
   });
 });
+
+function validateInputs() {
+  let emtpyCheck = checkEmpty();
+  let pwCheck = validatePw();
+  let pwCompare = comparePasswords();
+  let validationPassed = false;
+
+  if (!emtpyCheck) {
+    statusElement.innerHTML = "Please fill out all fields.";
+  } else if (!pwCheck) {
+    pwInput.style.borderColor = "red";
+    statusElement.innerHTML = "Password needs to contain at least 8 characters";
+  } else if (!pwCompare) {
+    pwInput.style.borderColor = "red";
+    pwRepeatInput.style.borderColor = "red";
+    statusElement.innerHTML = "Passwords need to match";
+  } else {
+    validationPassed = true;
+  }
+  return validationPassed;
+}
+
+function checkEmpty() {
+  let bool = true;
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value === "") {
+      input.style.borderColor = "red";
+      bool = false;
+    } else {
+      input.style.borderColor = "gray";
+    }
+  });
+  return bool;
+}
+
+function validatePw() {
+  const pw = document.getElementById("pw").value;
+  if (pw.length >= 8) {
+    return true;
+  }
+
+  return false;
+}
+
+pwInput.addEventListener("keyup", (e) => {
+  const pwLabel = document.getElementById("pw-label");
+  if (e.target.value.length < 8) {
+    pwLabel.innerHTML = "Password - Needs to be at least 8 characters long";
+    pwLabel.style.color = "red";
+  } else {
+    pwLabel.style.color = "black";
+    pwLabel.innerHTML = "Password";
+  }
+  comparePasswords();
+});
+
+pwRepeatInput.addEventListener("keyup", (e) => {
+  comparePasswords();
+});
+
+function comparePasswords() {
+  const pwRepeatLabel = document.getElementById("repeatpw-label");
+  if (pwRepeatInput.value !== "") {
+    if (pwRepeatInput.value !== pwInput.value) {
+      pwRepeatLabel.style.color = "red";
+      pwRepeatLabel.innerHTML = "Repeat Password - Passwords do not match!";
+    } else {
+      pwRepeatLabel.style.color = "black";
+      pwRepeatLabel.innerHTML = "Repeat Password";
+      return true;
+    }
+  }
+
+  return false;
+}
